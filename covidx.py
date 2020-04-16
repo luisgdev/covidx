5bc4e4a35867b844483bfc07c7081e11b118f439
Luis Ch <45129217+veluis@users.noreply.github.com>
2020-04-16 19:20:03 -0400

    Elapsed timer enhanced

--- covidx.py
+++ covidx.py
@@ -2,66 +2,70 @@ import sys
 import time
 import requests
 
+def run():
+    # Source URL
+    url = 'https://corona.lmao.ninja/v2/'
 
-print('.')
-# Start counting elapsed time
-init_time = time.perf_counter()
+    # Set URL endpoint
+    if len(sys.argv) == 1:
+        url += 'all'
+    else:
+        country = sys.argv[1]
+        print(f'==== COVID19 IN {country.upper()} ====')
+        url += f'countries/{country}'
 
-# Source URL
-url = 'https://corona.lmao.ninja/v2/'
+    # Request data
+    data = requests.get(url).json()
 
-# Set URL endpoint
-if len(sys.argv) == 1:
-    url += 'all'
-else:
-    country = sys.argv[1]
-    print(f'==== COVID19 IN {country.upper()} ====')
-    url += f'countries/{country}'
+    # Process data
+    cases = data['cases']
+    deaths = data['deaths']
+    recovered = data['recovered']
+    deathr = round(deaths/cases*100, 2)
+    recovr = round(recovered/cases*100, 2)
+    tests = data['tests']
+    todayCases = data['todayCases']
+    todayDeaths = data['todayDeaths']
+    active = data['active']
+    critical = data['critical']
+    casesPerOneMillion = data['casesPerOneMillion']
+    deathsPerOneMillion = data['deathsPerOneMillion']
+    testsPerOneMillion = data['testsPerOneMillion']
+    if len(sys.argv) == 1:
+        affectedCountries = data['affectedCountries']
+        print('===== COVID19 GLOBAL DATA =====')
 
-# Request data
-data = requests.get(url).json()
+    # Print processed data
+    print(f'Total Cases: {cases}')
+    print(f'Deaths: {deaths} ({deathr}%)')
+    print(f'Recovered: {recovered} ({recovr}%)')
+    print('-------------------------')
+    print(f'Today Cases: {todayCases}')
+    print(f'Today Deaths: {todayDeaths}')
+    print('-------------------------')
+    print(f'Active Cases: {active}')
+    print(f'Critical Cases: {critical}')
+    print('-------------------------')
+    print(f'Total Tests: {tests}')
+    print(f'Tests per Million: {testsPerOneMillion}')
+    print(f'Cases per Million: {casesPerOneMillion}')
+    print(f'Deaths per Million: {deathsPerOneMillion}')
+    print('-------------------------')
+    if len(sys.argv) == 1:
+        print(f'Affected Countries: {affectedCountries}')
+        print('=========================')
 
-# Process data
-cases = data['cases']
-deaths = data['deaths']
-recovered = data['recovered']
-deathr = round(deaths/cases*100, 2)
-recovr = round(recovered/cases*100, 2)
-tests = data['tests']
-todayCases = data['todayCases']
-todayDeaths = data['todayDeaths']
-active = data['active']
-critical = data['critical']
-casesPerOneMillion = data['casesPerOneMillion']
-deathsPerOneMillion = data['deathsPerOneMillion']
-testsPerOneMillion = data['testsPerOneMillion']
-if len(sys.argv) == 1:
-    affectedCountries = data['affectedCountries']
-    print('===== COVID19 GLOBAL DATA =====')
 
-# Print processed data
-print(f'Total Cases: {cases}')
-print(f'Deaths: {deaths} ({deathr}%)')
-print(f'Recovered: {recovered} ({recovr}%)')
-print('-------------------------')
-print(f'Today Cases: {todayCases}')
-print(f'Today Deaths: {todayDeaths}')
-print('-------------------------')
-print(f'Active Cases: {active}')
-print(f'Critical Cases: {critical}')
-print('-------------------------')
-print(f'Total Tests: {tests}')
-print(f'Tests per Million: {testsPerOneMillion}')
-print(f'Cases per Million: {casesPerOneMillion}')
-print(f'Deaths per Million: {deathsPerOneMillion}')
-print('-------------------------')
-if len(sys.argv) == 1:
-    print(f'Affected Countries: {affectedCountries}')
-    print('=========================')
-
-# Start counted elapsed time
-print(f' *** Elapsed time: {round(time.perf_counter() - init_time, 2)} s ***\n.')
+def test():
+    print('.')
+    # Start counting elapsed time
+    init_time = time.perf_counter()
+    # Run program
+    run()
+    # Stopt counting elapsed time
+    elapsed = round(time.perf_counter() - init_time, 2)
+    print(f' *** Elapsed time: {elapsed} s ***\n.')
 
 
 if __name__ == "__main__":
-    pass
+    test()
