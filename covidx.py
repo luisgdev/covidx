1405ac5fa1449888f170a79d2d55f335be29184f
Luis Ch <g298luis@gmail.com>
2022-02-25 23:21:45 -0400

    Black refactor

--- covidx.py
+++ covidx.py
@@ -1,70 +1,72 @@
 import sys
 import time
+
 import requests
+
 
 def run():
     # Source URL
-    url = 'https://corona.lmao.ninja/v2/'
+    url = "https://corona.lmao.ninja/v2/"
 
     # Set URL endpoint
     if len(sys.argv) == 1:
-        url += 'all'
-        print('===== COVID19 GLOBAL DATA =====')
+        url += "all"
+        print("===== COVID19 GLOBAL DATA =====")
     else:
         country = sys.argv[1]
-        print(f'==== COVID19 IN {country.upper()} ====')
-        url += f'countries/{country}'
+        print(f"==== COVID19 IN {country.upper()} ====")
+        url += f"countries/{country}"
 
     # Request data
     data = requests.get(url).json()
 
     # Process data
-    cases = data['cases']
-    deaths = data['deaths']
-    recovered = data['recovered']
-    deathr = round(deaths/cases*100, 2)
-    recovr = round(recovered/cases*100, 2)
-    tests = data['tests']
-    todayCases = data['todayCases']
-    todayDeaths = data['todayDeaths']
-    active = data['active']
-    critical = data['critical']
-    casesPerOneMillion = data['casesPerOneMillion']
-    deathsPerOneMillion = data['deathsPerOneMillion']
-    testsPerOneMillion = data['testsPerOneMillion']
+    cases = data["cases"]
+    deaths = data["deaths"]
+    recovered = data["recovered"]
+    deathr = round(deaths / cases * 100, 2)
+    recovr = round(recovered / cases * 100, 2)
+    tests = data["tests"]
+    todayCases = data["todayCases"]
+    todayDeaths = data["todayDeaths"]
+    active = data["active"]
+    critical = data["critical"]
+    casesPerOneMillion = data["casesPerOneMillion"]
+    deathsPerOneMillion = data["deathsPerOneMillion"]
+    testsPerOneMillion = data["testsPerOneMillion"]
     if len(sys.argv) == 1:
-        affectedCountries = data['affectedCountries']
+        affectedCountries = data["affectedCountries"]
 
     # Print processed data
-    print(f'Total Cases: {cases}')
-    print(f'Deaths: {deaths} ({deathr}%)')
-    print(f'Recovered: {recovered} ({recovr}%)')
-    print('-------------------------')
-    print(f'Today Cases: {todayCases}')
-    print(f'Today Deaths: {todayDeaths}')
-    print('-------------------------')
-    print(f'Active Cases: {active}')
-    print(f'Critical Cases: {critical}')
-    print('-------------------------')
-    print(f'Total Tests: {tests}')
-    print(f'Tests per Million: {testsPerOneMillion}')
-    print(f'Cases per Million: {casesPerOneMillion}')
-    print(f'Deaths per Million: {deathsPerOneMillion}')
-    print('-------------------------')
+    print(f"Total Cases: {cases}")
+    print(f"Deaths: {deaths} ({deathr}%)")
+    print(f"Recovered: {recovered} ({recovr}%)")
+    print("-------------------------")
+    print(f"Today Cases: {todayCases}")
+    print(f"Today Deaths: {todayDeaths}")
+    print("-------------------------")
+    print(f"Active Cases: {active}")
+    print(f"Critical Cases: {critical}")
+    print("-------------------------")
+    print(f"Total Tests: {tests}")
+    print(f"Tests per Million: {testsPerOneMillion}")
+    print(f"Cases per Million: {casesPerOneMillion}")
+    print(f"Deaths per Million: {deathsPerOneMillion}")
+    print("-------------------------")
     if len(sys.argv) == 1:
-        print(f'Affected Countries: {affectedCountries}')
-        print('=========================')
+        print(f"Affected Countries: {affectedCountries}")
+        print("=========================")
 
 
 def test():
-    print('.')
+    print(".")
     # Start counting elapsed time
     init_time = time.perf_counter()
     # Run program
     run()
     # Stopt counting elapsed time
     elapsed = round(time.perf_counter() - init_time, 2)
-    print(f' *** Elapsed time: {elapsed} s ***\n.')
+    print(f" *** Elapsed time: {elapsed} s ***\n.")
 
 
 if __name__ == "__main__":
